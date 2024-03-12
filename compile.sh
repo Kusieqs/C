1a0ee78ca37ed0b105cf5aabaefb3752c522a2a4
Kusieqs <130232383+Kusieqs@users.noreply.github.com>
2024-03-12 08:18:25 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,13 +5,11 @@ find . -name '*.c' | while read file; do
     if [ -f "$file" ]; then
         filename=$(basename "$file" .c)
         output_path=$(dirname "$file")
-        clang -o "${output_path}/${filename}" "$file" -lm
-        clang -o "${output_path}/${filename}.exe" "$file" -lm
+        clang -o "${output_path}/${filename}" "$file"
         if [ $? -eq 0 ]; then
             echo "Compilation successful for $file"
         else
             echo "Error compiling $file"
-            exit 1 # Exit the pipeline with a non-zero status code
         fi
     fi
 done
